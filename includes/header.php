073b4907ffd5713e40a46cac3b04e60d4cb88481
ma0216 <ma0216@mix.wvu.edu>
2020-11-04 13:35:13 +0500

    Update index.php

--- includes/header.php
+++ includes/header.php
@@ -23,7 +23,7 @@ session_start();
     <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" href="about.php">Home</a>
+          <a class="nav-link" href="index.php">Home</a>
         </li>
          <?php
           if (isset($_SESSION['uid'])) {
